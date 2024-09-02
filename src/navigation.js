35f2a1553f29df47baf3ed95e15aa5a021a3f399
Miguel Angel Arias <miguel.a.frontend@gmail.com>
2024-09-02 10:00:40 -0500

    (function navigator: cambio de condicionales if por operador ternario)

--- src/navigation.js
+++ src/navigation.js
@@ -2,17 +2,17 @@ window.addEventListener('DOMContentLoaded', navigator, false);
 window.addEventListener('hashchange', navigator, false);
 
 function navigator() {
-    if(location.hash.startsWith('#trends')) {
-        trendsPage();
-    } else if(location.hash.startsWith('#search=')) {
-        searchPage();
-    } else if(location.hash.startsWith('#movie=')) {
-        moviePage();
-    } else if(location.hash.startsWith('#category=')) {
-        categoryPage();
-    } else {
-        homePage();
-    }
+
+    location.hash.startsWith('#trends')     ? 
+    trendsPage()    :
+    location.hash.startsWith('#search=')    ?
+    searchPage()    :
+    location.hash.startsWith('#category=')  ?
+    categoryPage()  :
+    location.hash.startsWith('#movie=')     ?
+    moviePage()     :
+    homePage();
+    
 }
 
 function trendsPage() {
